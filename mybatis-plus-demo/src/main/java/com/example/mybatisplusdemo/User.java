157245eadc94b14c1691e12280ba5124d4ace823
黄湘豫 <huang_xiangyu@gzhc365.com>
2020-01-26 22:41:20 +0800

    jpa demo

---
+++ mybatis-plus-demo/src/main/java/com/example/mybatisplusdemo/User.java
@@ -0,0 +1,15 @@
+package com.example.mybatisplusdemo;
+
+import lombok.Data;
+
+/**
+ * @author chenjing
+ * @date 2020-01-22 11:23
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ mybatis-plus-demo/src/main/java/com/example/mybatisplusdemo/UserMapper.java
@@ -0,0 +1,10 @@
+package com.example.mybatisplusdemo;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author chenjing
+ * @date 2020-01-22 11:24
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
